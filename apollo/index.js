0778cc8cb5fb998c8078ec6d10e01379b3261df2
Griffin Tallant <griffin.tallant@gmail.com>
2020-06-12 07:10:24 -0600

    Added component library

--- apollo/index.js
+++ apollo/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import { ApolloServer, gql } from 'apollo-server-express';
+
 import typeDefs from './schema/schema';
 import resolvers from './schema/resolvers';
 
 const app = express();
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.STORAGE_URL, { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log(`Connected to mongo at ${process.env.STORAGE_URL}`));
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
@@ -11,5 +18,5 @@ server.applyMiddleware({ app });
 
 const PORT = process.env.PORT || 3000;
 app.listen({ port: PORT }, () => {
-    console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
-})
+    console.log(`Server ready at http://localhost:3000${server.graphqlPath}`);
+});
