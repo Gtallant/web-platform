b307e1168bdd5fd4f7520bf85a38ec7b3afb80f7
Griffin Tallant <griffin.tallant@gmail.com>
2020-06-29 20:58:29 -0600

    Added knobs to existing stories

--- component-library/src/atoms/headline/headline.stories.js
+++ component-library/src/atoms/headline/headline.stories.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { withKnobs, text } from "@storybook/addon-knobs";
 import Headline from './headline';
 
 export default {
   component: Headline,
   title: 'Atoms | Headline',
+  decorators: [withKnobs],
 };
 
-export const level_one = () => (<Headline level={1}>Hello World!</Headline>);
-export const level_two = () => (<Headline level={2}>Hello World!</Headline>);
-export const level_three = () => (<Headline level={3}>Hello World!</Headline>);
-export const level_four = () => (<Headline level={4}>Hello World!</Headline>);
-export const level_five = () => (<Headline level={5}>Hello World!</Headline>);
-export const level_six = () => (<Headline level={6}>Hello World!</Headline>);
+export const level_one = () => (<Headline level={1}>{text("Headline Text", "Hello World!")}</Headline>);
+export const level_two = () => (<Headline level={2}>{text("Headline Text", "Hello World!")}</Headline>);
+export const level_three = () => (<Headline level={3}>{text("Headline Text", "Hello World!")}</Headline>);
+export const level_four = () => (<Headline level={4}>{text("Headline Text", "Hello World!")}</Headline>);
+export const level_five = () => (<Headline level={5}>{text("Headline Text", "Hello World!")}</Headline>);
+export const level_six = () => (<Headline level={6}>{text("Headline Text", "Hello World!")}</Headline>);
