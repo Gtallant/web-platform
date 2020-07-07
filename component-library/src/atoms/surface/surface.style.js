d5048b42709ed5afbcc78f81b4addf478952acd0
Griffin Tallant <griffin.tallant@gmail.com>
2020-07-07 10:00:55 -0600

    Added card component

--- component-library/src/atoms/surface/surface.style.js
+++ component-library/src/atoms/surface/surface.style.js
@@ -8,7 +8,7 @@ function createShadow(elevation) {
   const numLevels = Math.min(elevation * 2.5 + 3, 25);
 
   for (let i = 1; i < numLevels; i++) {
-    const e = i * 3;
+    const e = i * 2;
     shadowCSS = `${shadowCSS},
     0 ${e + 5}px ${Math.abs(2.5*e - 3)}px rgba(0, 0, 0, ${Math.min(.33, 1 - i/numLevels)})
     `;
@@ -19,6 +19,7 @@ function createShadow(elevation) {
 
 export default styled.div`
   position: relative;
+  width: 100%;
   z-index: ${props => props.elevation};
   background: ${props => props.theme.colors.surface};
   opacity: .25;
