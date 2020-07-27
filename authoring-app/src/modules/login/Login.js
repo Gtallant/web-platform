890b57a903e409913a35c29d5ffcc080fd59d691
Griffin Tallant <griffin.tallant@gmail.com>
2020-07-27 09:17:47 -0600

    Fixed redux test

--- authoring-app/src/modules/login/Login.js
+++ authoring-app/src/modules/login/Login.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch  } from 'react-redux';
 import { Card, Button } from 'component-library';
 import { testAction } from './dux/loginActions';
 
 function Login() {
+  const dispatch = useDispatch();
+  const click = () => {
+    console.log('You clicked the login button');
+    console.log(typeof testAction);
+    dispatch(testAction("this is a payload"));
+  }
+
   return (
     <Card>
-      <Button onClick={() => testAction()} level={2}>Test Redux</Button>
+      <Button onClick={() => click()} level={2}>Test Redux</Button>
     </Card>
   );
 }
 
-const mapDispatchToProps = dispatch => ({
-  testAction: () => dispatch(testAction())
-});
-
-const mapStateToProps = state => {return {}};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
--- authoring-app/src/modules/login/dux/loginActions.js
+++ authoring-app/src/modules/login/dux/loginActions.js
@@ -1,9 +1,17 @@
-import { TEST_ACTION } from './loginActionTypes';
+import { TEST_ACTION, LOGIN } from './loginActionTypes';
 
-export const testAction = () => dispatch => {
+export const testAction = payload => dispatch => {
+  console.log(payload);
   dispatch({
    type: TEST_ACTION,
    payload: {
      auth: 'result_of_simple_action'}
   })
+ }
+
+ export const loginAttempt = payload => dispatch => {
+  dispatch({
+   type: LOGIN,
+   payload: payload
+  });
  }
